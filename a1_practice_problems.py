fa4976e93f442c35c63881358ff8fe008e8ad478
Allison Duran <aduran51@cps.edu>
2025-09-11 11:17:16 -0500

    finished mean and median

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -106,7 +106,10 @@ def mean(lst: List[int]) -> float:
     Returns:
         the mean of the passed in list
     """
-    raise NotImplementedError("mean")
+    #if len(lst) == 0:
+    #    return 0
+    return sum_list(lst) / len(lst) if lst else 0
+
 
 
 def median(lst: List[int]) -> float:
@@ -121,7 +124,11 @@ def median(lst: List[int]) -> float:
     Returns:
         the median of the passed in list
     """
-    raise NotImplementedError("median")
+    if len(lst) % 2 == 1:
+        return lst[len(lst) // 2]
+    else:
+        return (lst[len(lst)//2] + lst[len(lst) // 2 - 1]) / 2
+    
 
 
 def duck_duck_goose(lst: List[str]) -> List[str]:
@@ -155,8 +162,9 @@ if __name__ == "__main__":
     assert sum_list([1, 2, 3]) == 6, "sum_list of [1,2,3] failed"
     assert sum_list([54,23,9,17]) ==103, "sum_list of [54,23,9,17] failed"
     assert mean([1, 2, 3, 4, 5]) == 3, "mean of [1,2,3,4,5] failed"
-    assert median([1, 2, 3, 4, 5]) == 3, "median of [1,2,3,4,5] failed"
-
+    assert mean([1, 2, 3, 4, 5]) == 3, "mean of [1,2,3,4,5] failed"
+    assert median([1, 2, 3, 4, 5, 6 ]) == 21/6 , "median of [1,2,3,4,5,6] failed"
+    assert median ([1,2,3,4,5,6]) == 3.5, "median of [1,2,3,4,5,6] failed"
     names = ["roscoe", "kim", "woz", "solin", "law", "remess"]
     assert duck_duck_goose(names) == ["roscoe", "law"]
 
